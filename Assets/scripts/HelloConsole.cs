d960225ed6b7d2c5c3ee3156cc33fc1cdc98be35
RILEY MARKS <2138071@Swansea.ac.uk>
2022-10-17 16:37:45 +0100

    Added Hello Console script

---
+++ Assets/scripts/HelloConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log("Hello Riley");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
